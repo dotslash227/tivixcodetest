f67403cf1e748f329b7051a1b77f2cdddd43f7d3
reydon227 <dhruvarora@delhinerds.com>
2019-10-11 17:18:35 +0530

    migration fix and unittests

--- classroom/migrations/0001_initial.py
+++ classroom/migrations/0001_initial.py
@@ -33,7 +33,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
                 ('subject', models.CharField(choices=[('Maths', 'Maths'), ('Chemistry', 'Chemistry'), ('Physics', 'Physics')], max_length=100)),
-                ('students', models.ManyToManyField(through='classroom.Remarks', to='class.Student')),
+                ('students', models.ManyToManyField(through='classroom.Remarks', to='classroom.Student')),
             ],
         ),
         migrations.AddField(
--- classroom/tests.py
+++ classroom/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Teacher, Student, Remarks
 
-# Create your tests here.
+class Classroom(TestCase):    
+    def setUp(self):
+        Teacher.objects.create(name="Amit Shah", subject="Maths")
+        Teacher.objects.create(name="Narendra Modi", subject="Physics")
+        Student.objects.create(name="Rahul Gandhi", age=45)
+        Student.objects.create(name="Mamta Banerjee", age=60)            
+
+    def test_has_teachers(self):        
+        rahul = Student.objects.get(name="Rahul Gandhi")
+        mamta = Student.objects.get(name="Mamta Banerjee")
+        modi = Teacher.objects.get(name="Narendra Modi")
+        amit = Teacher.objects.get(name="Amit Shah")
+
+        modi.students.add(rahul)
+        amit.students.add(mamta)
+
+        self.assertTrue(rahul.teacher_set.all, "Rahul has teachers")
+        self.assertTrue(mamta.teacher_set.all, "Mamta has teachers")
+
+    def test_student_is_favorite(self):
+        rahul = Student.objects.get(name="Rahul Gandhi")
+        modi = Teacher.objects.get(name="Narendra Modi")
+
+        modi.students.add(rahul, through_defaults={"favorite":True})
+        remark = Remarks.objects.get(student=rahul)
+
+        self.assertTrue(remark.favorite, "The student is favorite")
+        self.assertFalse(remark.favorite, "Yes, the student is favorite") #This is supposed to fail. Worrysome if it doesn't fail.
