5c6c4f0e7528d40ecf84e80c132a76551c995023
reydon227 <dhruvarora@delhinerds.com>
2019-10-11 12:32:15 +0530

    class app setup, basic models

---
+++ class/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+SUBJECTS = (
+    ("Maths", "Maths"),
+    ("Chemistry", "Chemistry"),
+    ("Physics", "Physics")
+)
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100, choices=SUBJECTS)
+    students = models.ManyToManyField(Student, through="Remarks")
+
+    def __str__(self):
+        return self.name
+
+class Remarks(models.Model):
+    student = models.ForeignKey(Student, related_name="student", on_delete=models.DO_NOTHING)
+    teacher = models.ForeignKey(Teacher, related_name="teacher", on_delete=models.DO_NOTHING)
+    favorite = models.BooleanField(default=False, choices=(
+        (True, "Yes"),
+        (False, "No")
+    ))
+
