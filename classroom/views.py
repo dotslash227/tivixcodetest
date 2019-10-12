ed68c4b5919b077ff462d7a6a569b6080b8a81d7
reydon227 <dhruvarora@delhinerds.com>
2019-10-12 13:38:20 +0530

    bug fix and new db

--- classroom/views.py
+++ classroom/views.py
@@ -37,7 +37,7 @@ def studentsList(request):
 def addStudentToTeacher(request, teacher_id):
     teacher = Teacher.objects.get(pk=teacher_id)
     student = Student.objects.get(pk=request.POST.get("student"))
-    fav = True if request.POST.get("fav") else False
+    fav = "True" if request.POST.get("fav") else "False"
 
     teacher.students.add(student, through_defaults={"favorite":fav})
 
@@ -46,7 +46,7 @@ def addStudentToTeacher(request, teacher_id):
 def updateFavorite(request, teacher_id, student_id):
     teacher = Teacher.objects.get(pk=teacher_id)
     student = Student.objects.get(pk=student_id)
-    fav = True if request.POST.get("fav-student") else False
+    fav = "True" if request.POST.get("fav-student") else "False"
     Remarks.objects.filter(teacher=teacher, student=student).update(favorite=fav)
 
     return redirect("class:teacher-view", teacher_id=teacher_id)
